ee7533272aedc014577e52f30fa074feab6c807b
Shivamxagrawal <agrawalshivam57@gmail.com>
2025-07-26 18:56:47 +0530

    front

--- twek/src/components/index.ts
+++
@@ -1,4 +0,0 @@
-// Export all components from this directory
-export { default as ContactForm } from './ContactForm';
-export { default as Navbar } from './Navbar';
-export { default as AuthFlow } from './AuthFlow';
